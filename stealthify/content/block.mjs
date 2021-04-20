ee46c2ee0834bf4991ce18a580930d26f4cb107a
Cookie Engineer <cookiengineer@protonmail.com>
2021-04-20 11:27:31 +0200

    :unicorn: Script Blocking via MutationObserver at document_start

---
+++ stealthify/content/block.mjs
@@ -0,0 +1,26 @@
+
+new MutationObserver((mutations) => {
+
+	mutations.forEach((mutation) => {
+
+		if (mutation.type === 'childList') {
+
+			Array.from(mutation.addedNodes).forEach((node) => {
+
+				if (node.tagName === 'SCRIPT') {
+					node.parentNode.removeChild(node);
+				} else if (node.textContent.startsWith('<!')) {
+					node.parentNode.removeChild(node);
+				}
+
+			});
+
+		}
+
+	});
+
+}).observe(document.documentElement, {
+	childList: true,
+	subtree:   true
+});
+
---
+++ stealthify/content/clean.mjs
@@ -0,0 +1,83 @@
+
+const EVENTS = [
+	'abort',
+	'blur',
+	'cancel',
+	'canplay',
+	'canplaythrough',
+	'change',
+	'click',
+	'close',
+	'contextmenu',
+	'cuechange',
+	'dblclick',
+	'durationchange',
+	'ended',
+	'error',
+	'focus',
+	'gotpointercapture',
+	'input',
+	'invalid',
+	'keydown',
+	'keypress',
+	'keyup',
+	'load',
+	'loadedmetadata',
+	'loadend',
+	'loadstart',
+	'lostpointercapture',
+	'mousedown',
+	'mouseenter',
+	'mouseleave',
+	'mousemove',
+	'mouseout',
+	'mouseover',
+	'mouseup',
+	'pause',
+	'play',
+	'playing',
+	'pointercancel',
+	'pointerdown',
+	'pointerenter',
+	'pointerleave',
+	'pointermove',
+	'pointerout',
+	'pointerover',
+	'pointerup',
+	'reset',
+	'resize',
+	'scroll',
+	'select',
+	'selectionchange',
+	'selectstart',
+	'submit',
+	'touchcancel',
+	'touchstart',
+	'transitioncancel',
+	'transitionend',
+	'wheel'
+];
+
+
+EVENTS.forEach((event) => {
+
+	let nodes = Array.from(document.querySelectorAll('*[on' + event + ']'));
+	if (nodes.length > 0) {
+
+		nodes.forEach((node) => {
+
+			node.setAttribute('on' + event, null);
+			node.removeAttribute('on' + event);
+
+			node['on' + event] = null;
+
+			setTimeout(() => {
+				node.replaceWith(node.cloneNode(true));
+			}, 0);
+
+		});
+
+	}
+
+});
+
